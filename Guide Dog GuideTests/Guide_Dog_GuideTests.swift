7ff6d824332e6e5152580adf86c23e35c405467b
David Bradford <david.bradford@yodle.com>
2017-01-14 11:04:54 -0600

    Initial Commit

---
+++ Guide Dog GuideTests/Guide_Dog_GuideTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guide_Dog_GuideTests.swift
+//  Guide Dog GuideTests
+//
+//  Created by David Bradford on 1/14/17.
+//  Copyright © 2017 David Bradford. All rights reserved.
+//
+
+import XCTest
+@testable import Guide_Dog_Guide
+
+class Guide_Dog_GuideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
